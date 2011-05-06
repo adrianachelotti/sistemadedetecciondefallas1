4207212add86fe8b6cb8629f3fd88d9fde20fc58
adrianachelotti <adrianachelotti@d96684e7-4314-671d-76f1-bdfcb5867f5c>
2011-05-06 03:48:13 +0000

    me habia olvidado de agregar los actions y el struts.xml

---
+++ trunk/motor_inferencia/tp_fallas/src/main/java/action/DiagnosticoAction.java
@@ -0,0 +1,12 @@
+package action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+@SuppressWarnings("serial")
+public class DiagnosticoAction extends ActionSupport {
+	
+	public String execute(){
+		return "success";
+	}
+
+}
